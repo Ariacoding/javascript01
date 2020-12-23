e8b79b50112df16847774c6afa6bfbd63faf72b4
Ariacoding <ariaheidari2010@gmail.com>
2020-12-22 19:15:24 -0800

    Complete all four operators

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-function handleClick() {
+function handleClickPlus() {
     const num1 = document.getElementById("number1").value;
     const num2 = document.getElementById("number2").value;
+    const result = Number(num1) + Number(num2);
+    document.getElementById("result").innerText = result;
 
+}
+function handleClickMinus() {
+    const num1 = document.getElementById("number1").value;
+    const num2 = document.getElementById("number2").value;
+    const result = (num1) - (num2);
+    document.getElementById("result").innerText = result;
+}
+function handleClickDivide() {
+    const num1 = document.getElementById("number1").value;
+    const num2 = document.getElementById("number2").value;
+    const result = (num1) / (num2);
+    const finalResult = (Math.round(result * 1000) / 1000).toFixed(3);
+    document.getElementById("result").innerText = finalResult;
+}
+function handleClickMultiply() {
+    const num1 = document.getElementById("number1").value;
+    const num2 = document.getElementById("number2").value;
+    const result = (num1) * (num2);
+    document.getElementById("result").innerText = result;
 }
